e4e4498c6fccf8d541af806011942508ed29509c
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-01-30 21:52:16 -0500

    clean up install script

--- scripts/dotFiles.py
+++ scripts/dotFiles.py
@@ -1,41 +1,58 @@
 #!/usr/bin/python
-import os,re
-
-def srcDestPair(src,dest):
-    if(re.search(re.compile("/$"),dest)):
-        dest = os.path.join(dest,os.path.basename(src))
-    return({'src':src, 'dest':dest})
+import os,re,sys
 
 home = os.path.expanduser("~/")
 srcDir = os.path.join(home,"systemConfig","dotFiles")
 destDir = home
-dotFiles = [".bash_profile",
-            ".profile",
-            ".bashrc",
-            ".emacs",
-            ".dircolors",
-            ".xbindkeysrc",
-            ".Xresources",
-            ".gitconfig",
-            ".gitignore_global",
-            ".toprc"]
-dotFiles = [ srcDestPair(os.path.join(srcDir,f),destDir) for f in dotFiles]
+if sys.platform == "darwin":
+    dotFiles = [(".bashrc_osx", ".bashrc"),
+                ".dircolors",
+                ".gitconfig",
+                ".gitignore_global"]
+elif sys.platform == "linux" or sys.platform == "linux2":
+    dotFiles = [".bash_profile",
+                ".profile",
+                (".bashrc_ubuntu", ".bashrc"),
+                ".dircolors",
+                ".Xresources",
+                ".gitconfig",
+                ".gitignore_global",
+                ".toprc"]
+else:
+    print "Invalid platform name: %s" % sys.platform
+    exit(1)
 
-if __name__ == "__main__":
-    if os.path.isdir(srcDir) :
-        for f in dotFiles:
-            print f
-            if os.path.islink(f['dest']):
-                print "unlinking " + f['dest']
-                os.unlink(f['dest'])
-            elif os.path.isfile(f['dest']):
-                print "renaming " + f['dest']
-                os.rename(f['dest'],f['dest']+".old")
 
-            os.symlink(f['src'],f['dest'])
-
+srcDestPairs = []
+for df in dotFiles:
+    if isinstance(df, tuple):
+        srcName = df[0]
+        destName = df[1]
     else:
-        print "Source directory \""+srcDir+"\" does not exist."
-        exit(1)
+        srcName = destName = df
 
-    exit(0)
+    srcName = os.path.join(srcDir, srcName)
+    destName = os.path.join(home, destName)
+
+    print "Installing: ", os.path.basename(srcName)
+    print "To: ",os.path.basename(destName)
+
+    if os.path.islink(destName):
+        print "removing existing link"
+        os.unlink(destName)
+    elif os.path.isfile(destName):
+        print "file exists"
+        ind = 1
+        newDestName + ".old"
+        while os.path.isfile(newDestName):
+            print "trying",newDestName
+
+            newDestName + "." + str(ind) + ".old"
+
+        if os.path.islink(newDestName):
+            print "removing link"
+            os.unlink(newDestName)
+
+        destName = newDestName
+
+    os.symlink(srcName, destName)
